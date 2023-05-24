cec2e63e394782ab5fe31fabc5406fb2e9665c13
YahirRdz <71789382+YahirRdz@users.noreply.github.com>
2023-05-24 12:18:48 -0600

    Setup Google Config

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react";
+
+import PrompsCard from "./PrompsCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      Texto
+      {data.map((post) => {
+        <PrompsCard
+          key={post.id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />;
+      })}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [searchText, setsearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const handleSearchChange = (e) => {};
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
 
-export default Feed
+      setPosts(data);
+    };
+    fetchPost();
+  }, []);
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center' action=''>
+        <input
+          placeholder='Ingresa'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          type='text'
+        />
+      </form>
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
